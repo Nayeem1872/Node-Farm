779d31ae25fe21ea628f33c5f98f059dce567cda
Nayeem1872 <nayeem.islam.1872@gmail.com>
2022-08-20 20:10:24 +0600

    Signed-off-by: Nayeem1872 <nayeem.islam.1872@gmail.com>

---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output= output.replace(/{%IMAGE%}/g, product.image);
+    output= output.replace(/{%PRICE%}/g, product.price);
+    output= output.replace(/{%FROM%}/g, product.from);
+    output= output.replace(/{%NUTNAME%}/g, product.nutname);
+    output= output.replace(/{%QUANTITY%}/g, product.quantity);
+    output= output.replace(/{%DESCRIPTION%}/g, product.description);
+    output= output.replace(/{%ID%}/g, product.id);
+    
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+
+
+
+const data = fs.readFileSync(`${__dirname}/new.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res)=>{
+    const pathName = req.url;
+    if(pathName==='/' || pathName === '/overview'){
+        res.writeHead(200,{'content-type':'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml); 
+        // console.log(cardsHtml);
+        res.end(output);
+    }
+    else if(pathName==='/product'){
+        res.end('This is product');
+    }
+    
+    else if (pathname === '/api') {
+        res.writeHead(200, {
+          'Content-type': 'application/json'
+        });
+        res.end(data);
+    
+        // Not found
+      } else {
+        res.writeHead(404, {
+          'Content-type': 'text/html',
+          'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+      }
+
+
+});
+server.listen(8000, '127.0.0.1',()=>{
+    console.log('Listening to requests on port 8000');
+});
